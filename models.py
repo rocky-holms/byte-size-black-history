25e2c3c5c9f24ef3840d4e287a60470c52e926ea
Joseph Rocky Holms <rocky@reivernet.com>
2020-07-28 12:13:15 -0400

    Changed model attribute.

--- models.py
+++ models.py
@@ -32,4 +32,4 @@ class WikiLink(Base):
 
     url = Column(URLType, nullable=False)
     title = Column(Text, nullable=False)
-    date_used = Column(ArrowType, nullable=False)
+    date_used = Column(ArrowType, nullable=True)
